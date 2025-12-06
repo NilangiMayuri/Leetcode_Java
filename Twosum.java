7d74fc0d6b7285263a16ddb7682954145dab573e
NilangiMayuri <165762875+NilangiMayuri@users.noreply.github.com>
2025-12-06 23:15:19 +0530

    Implement twoSum method in Solution class

---
+++ Twosum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> dict = new HashMap<>();
+        int n = nums.length;
+
+        for(int i = 0; i<n ; i++){
+            int t = target-nums[i];
+
+            if(dict.containsKey(t)){
+                int[] res = new int[]{dict.get(t), i}; 
+            return res;
+            } else {
+                dict.put(nums[i], i);
+            }
+        }
+        return new int[]{};
+    }
+}
